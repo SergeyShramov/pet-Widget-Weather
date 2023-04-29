c2fdfc64d49088174eccbd72d2e6cbce7896ff78
SergeyShramov <sergshramik@gmail.com>
2023-04-29 15:33:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,110 @@
+const apiKey = "10207f8e3592fa7edbd68c9ba9cfa61b";
+const forecastWeather = `https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=${apiKey}`;
+
+let cityWeather = document.querySelector(".weather-item");
+let cityWeatherCloud = document.querySelector(".weather-cloud")
+let cloudIcon = document.querySelector(".cloud");
+let cityName = document.querySelector(".city");
+let weatherDate = document.querySelector('.weather-five-day');
+
+window.addEventListener('load', (event) => {
+    const url = forecastWeather.replace("{city name}", 'Warsaw');
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            cityName.innerHTML = data.city.name;
+            cityWeather.innerHTML = Math.round(data.list[0].main.temp - 273.15) + '&#176';
+            cityWeatherCloud.innerHTML = data.list[0].weather[0]['description'];
+            cloudIcon.innerHTML = '<img src="https://openweathermap.org/img/wn/' + data.list[0].weather[0]['icon'] + '@2x.png">';
+        })
+        .catch(error => console.log(error));
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let forecastHtml = "";
+
+            for (let i = 0; i < data.list.length; i += 8) {
+                const day = data.list[i];
+                const date = new Date(day.dt * 1000);
+                const iconUrl = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+                const description = day.weather[0].description;
+                const temp = Math.round(day.main.temp - 273.15); // Convert temperature to Celsius
+
+                forecastHtml += `
+            <div class="weather-day">
+              <div class="weather-date">${date.toLocaleDateString()}</div>
+              <div class="weather-icon"><img src="${iconUrl}" alt="${description}" /></div>
+              <div class="weather-description">${description}</div>
+              <div class="weather-temp">${temp}°C</div>
+            </div>
+          `;
+            }
+            weatherDate.innerHTML = forecastHtml;
+        })
+        .catch(error => {
+            console.error("Error fetching weather data:", error);
+        });
+
+})
+
+const form = document.querySelector("#city-form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const cityInput = document.querySelector("#city-input");
+    const city = cityInput.value;
+
+    if (city.length > 0) {
+        const url = forecastWeather.replace("{city name}", encodeURIComponent(city));
+        fetch(url)
+            .then(response => response.json())
+            .then((data) => {
+                cityName.innerHTML = data.city.name;
+                cityWeather.innerHTML = Math.round(data.list[0].main.temp - 273.15) + '&#176';
+                cityWeatherCloud.innerHTML = data.list[0].weather[0]['description'];
+                cloudIcon.innerHTML = '<img src="https://openweathermap.org/img/wn/' + data.list[0].weather[0]['icon'] + '@2x.png">';
+            })
+            .catch(error => console.log(error));
+    }
+
+})
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const cityInput = document.querySelector("#city-input");
+    const city = cityInput.value;
+
+    if (city.length > 0) {
+        const url = forecastWeather.replace("{city name}", encodeURIComponent(city));
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                let forecastHtml = "";
+
+                for (let i = 0; i < data.list.length; i += 8) {
+                    const day = data.list[i];
+                    const date = new Date(day.dt * 1000);
+                    const iconUrl = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+                    const description = day.weather[0].description;
+                    const temp = Math.round(day.main.temp - 273.15); // Convert temperature to Celsius
+
+                    forecastHtml += `
+            <div class="weather-day">
+              <div class="weather-date">${date.toLocaleDateString()}</div>
+              <div class="weather-icon"><img src="${iconUrl}" alt="${description}" /></div>
+              <div class="weather-description">${description}</div>
+              <div class="weather-temp">${temp}°C</div>
+            </div>
+          `;
+                }
+                weatherDate.innerHTML = forecastHtml;
+            })
+            .catch(error => {
+                console.error("Error fetching weather data:", error);
+            });
+
+        cityInput.value = "";
+    }
+});
